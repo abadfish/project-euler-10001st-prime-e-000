81d4658c3f3c1c1adac3f279ed351176fc16de99
abadfish <kirsten.ofarrell@gmail.com>
2015-12-16 15:11:58 -0500

    Done.

--- lib/10001st_prime.rb
+++ lib/10001st_prime.rb
@@ -1,1 +1,7 @@
 # Implement your procedural solution here!
+require 'prime'
+
+def prime_number_for(nth)
+  Prime.first(nth).max
+end
+
--- lib/oo_10001st_prime.rb
+++ lib/oo_10001st_prime.rb
@@ -1,1 +1,22 @@
 # Implement your object-oriented solution here!
+class Prime
+
+  def initialize(nth)
+    @nth = nth
+  end
+
+  def nth(nth)
+    @nth = nth
+  end
+
+  def nth
+    @nth
+  end
+
+  require 'prime'
+
+  def number
+    Prime.first(@nth).max
+  end
+
+end
